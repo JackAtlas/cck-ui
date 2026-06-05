3641f928369593e6834f313af8d8307362a161f0
JackAtlas <jackatlas8864@gmail.com>
2026-06-05 20:48:51 +0800

    feat(util): util functions for setting `install` function of components

---
+++ packages/utils/vue/install.ts
@@ -0,0 +1,54 @@
+import { hasOwn, isArray } from '@vue/shared'
+import { isPlainObject } from 'es-toolkit'
+import type { SFCWithInstall } from './typescript'
+
+export const withPropsDefaultSetter = (target: any) => {
+  const _p = target.props
+  const props = isArray(_p)
+    ? Object.fromEntries(_p.map((key) => [key, {}]))
+    : _p
+
+  target.setPropsDefaults = (defaults: Record<string, any>) => {
+    if (!props) return
+
+    for (const [key, value] of Object.entries(defaults)) {
+      const prop = props[key]
+
+      if (!hasOwn(props, key)) continue
+
+      if (isPlainObject(prop)) {
+        props[key] = {
+          ...props,
+          default: value
+        }
+        continue
+      }
+
+      props[key] = {
+        type: prop,
+        default: value
+      }
+    }
+
+    target.props = props
+  }
+}
+
+export const withInstall = <T, E extends Record<string, any>>(
+  main: T,
+  extra?: E
+) => {
+  ;(main as SFCWithInstall<T>).install = (app): void => {
+    for (const comp of [main, ...Object.values(extra ?? [])]) {
+      app.component(comp.name, comp)
+    }
+  }
+
+  if (extra) {
+    for (const [key, comp] of Object.entries(extra)) {
+      ;(main as any)[key] = comp
+    }
+  }
+  withPropsDefaultSetter(main)
+  return main as SFCWithInstall<T> & E
+}
---
+++ packages/utils/vue/typescript.ts
@@ -0,0 +1,68 @@
+import type {
+  AllowedComponentProps,
+  AppContext,
+  Component,
+  EmitsOptions,
+  ObjectPlugin,
+  SetupContext,
+  VNodeProps
+} from 'vue'
+import {
+  type ComponentEmit,
+  type ComponentProps
+} from 'vue-component-type-helpers'
+
+type NativeType =
+  | null
+  | undefined
+  | number
+  | string
+  | boolean
+  | symbol
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  | Function
+
+type InferDefaults<T> = {
+  [K in keyof T as string extends K ? never : K]?: InferDefault<T[K]>
+}
+
+type InferDefault<T> = (() => T & {}) | (T extends NativeType ? T : never)
+
+type ExtractEventNames<T> =
+  ComponentEmit<T> extends (event: string, ...args: any[]) => any
+    ? never
+    : keyof {
+        [K in keyof ComponentProps<T> as K extends `on${infer Event}`
+          ? ComponentEmit<T> extends (
+              event: Uncapitalize<Event>,
+              ...args: any[]
+            ) => any
+            ? K
+            : never
+          : never]: unknown
+      }
+
+type ExcludeProps<T> =
+  | ExtractEventNames<T>
+  | keyof VNodeProps
+  | keyof AllowedComponentProps
+
+export type SFCWithInstall<T> = T & ObjectPlugin & SFCWithPropsDefaultSetter<T>
+
+export type SFCInstallWithContext<T> = SFCWithInstall<T> & {
+  _context: AppContext | null
+}
+
+export type SFCWithPropsDefaultSetter<T> = T extends Component
+  ? {
+      setPropsDefault: (
+        defaults: InferDefaults<{
+          [K in keyof ComponentProps<T> as K extends ExcludeProps<T>
+            ? never
+            : K]?: ComponentProps<T>[K]
+        }>
+      ) => void
+    }
+  : unknown
+
+export type EmitFn<E extends EmitsOptions> = SetupContext<E>['emit']
