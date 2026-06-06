d1bf49d66961c69015e9d52235184f4375d72429
JackAtlas <jackatlas8864@gmail.com>
2026-06-06 11:14:04 +0800

    docs(theme): default theme

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <DefaultTheme.Layout />
+</template>
+
+<script setup lang="ts">
+import { useData } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+import { watch } from 'vue'
+
+const { isDark } = useData()
+
+watch(
+  isDark,
+  (newIsDark) => {
+    document.documentElement.setAttribute(
+      'data-c-color-scheme',
+      newIsDark ? 'dark' : 'light'
+    )
+  },
+  { immediate: true }
+)
+</script>
---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,14 @@
+import { h } from 'vue'
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+import Layout from './Layout.vue'
+import CckUI from 'cck-ui'
+import '@cck-ui/theme-slate'
+
+export default {
+  extends: DefaultTheme,
+  Layout,
+  enhanceApp({ app, router, siteData }) {
+    app.use(CckUI)
+  }
+} satisfies Theme
