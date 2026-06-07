34eaf5e6a365dd66f2f845ecbac653e9898c416a
JackAtlas <jackatlas8864@gmail.com>
2026-06-07 20:31:49 +0800

    feat(docs): change style url to published package

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,8 @@
-import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue'
 import CckUI from 'cck-ui'
-import '@cck-ui/theme-slate'
+import 'cck-ui/dist/index.css'
 
 export default {
   extends: DefaultTheme,
