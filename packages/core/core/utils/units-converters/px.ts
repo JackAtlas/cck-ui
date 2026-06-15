f669619bab79f18c7b24a44843551f1e5617fa2d
JackAtlas <jackatlas8864@gmail.com>
2026-06-15 11:47:01 +0800

    feat(utils): unis converter utils

---
+++ packages/core/core/utils/units-converters/px.ts
@@ -0,0 +1,33 @@
+function getTransformdScaledValue(value: unknown) {
+  if (typeof value !== 'string' || !value.includes('var(--c-scale)'))
+    return value
+
+  return value
+    .match(/^calc\((.*?)\)$/)?.[1]
+    .split('*')[0]
+    .trim()
+}
+
+export function px(value: unknown) {
+  const transformedValue = getTransformdScaledValue(value)
+
+  if (typeof transformedValue === 'number') return transformedValue
+
+  if (typeof transformedValue === 'string') {
+    if (transformedValue.includes('calc') || transformedValue.includes('var'))
+      return transformedValue
+
+    if (transformedValue.includes('px'))
+      return Number(transformedValue.replace('px', ''))
+
+    if (transformedValue.includes('rem'))
+      return Number(transformedValue.replace('rem', '')) * 16
+
+    if (transformedValue.includes('em'))
+      return Number(transformedValue.replace('em', '')) * 16
+
+    return Number(transformedValue)
+  }
+
+  return Number.NaN
+}
