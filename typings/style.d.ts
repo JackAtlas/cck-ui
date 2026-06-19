47a1f66ed8b10d2b73e78566753f3629e4457b82
JackAtlas <jackatlas8864@gmail.com>
2026-06-19 19:10:07 +0800

    chore: typescript support module css

--- typings/style.d.ts
+++ typings/style.d.ts
@@ -1,2 +1,8 @@
-declare module '*.css' {}
-declare module '*.scss' {}
+declare module '*.css' {
+  const classes: { [className: string]: string }
+  export default classes
+}
+declare module '*.scss' {
+  const classes: { [className: string]: string }
+  export default classes
+}
