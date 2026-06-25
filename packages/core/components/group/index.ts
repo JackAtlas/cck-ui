6631c33f8da6d5480cc542a17e44b7088923f506
JackAtlas <jackatlas8864@gmail.com>
2026-06-25 11:26:42 +0800

    refactor(group): restructure group files

--- packages/core/components/group/index.ts
+++ packages/core/components/group/index.ts
@@ -1,7 +1,7 @@
-import { SFCWithInstall, withInstall } from '@cck-ui/core/core'
-import Group from './src/group.vue'
+import { SFCWithInstall, withInstall } from '@cck-ui/core'
+import Group from './group.vue'
 
 export const CGroup: SFCWithInstall<typeof Group> = withInstall(Group)
 export default CGroup
 
-export * from './src/group.types'
+export * from './group.types'
