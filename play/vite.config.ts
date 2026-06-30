a07e1f737d95d6dd0f1ae5fe725c0e75ed57e372
JackAtlas <jackatlas8864@gmail.com>
2026-06-30 20:45:10 +0800

    chore(deps): update deps

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@cck-ui/core': path.resolve(__dirname, '../packages/@cck-ui/core/src/index.ts'),
+    },
+  },
 })
