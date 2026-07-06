8a19d59ee81736c3ef106dfcc9e422ec8c30d55d
JackAtlas <jackatlas8864@gmail.com>
2026-07-06 18:27:57 +0800

    style: code format

--- packages/@cck-ui/core/src/core/box/get-box-mod/get-box-mod.ts
+++ packages/@cck-ui/core/src/core/box/get-box-mod/get-box-mod.ts
@@ -8,12 +8,7 @@ export function getMod(props: Record<string, any>) {
   return Object.keys(props).reduce<Record<string, any>>((acc, key) => {
     const value = props[key]
 
-    if (
-      value === undefined ||
-      value === '' ||
-      value === false ||
-      value === null
-    ) {
+    if (value === undefined || value === '' || value === false || value === null) {
       return acc
     }
 
@@ -23,7 +18,9 @@ export function getMod(props: Record<string, any>) {
 }
 
 export function getBoxMod(mod?: any): Record<string, any> | null {
-  if (!mod) return null
+  if (!mod) {
+    return null
+  }
 
   if (typeof mod === 'string') {
     return { [transformModKey(mod)]: true }
