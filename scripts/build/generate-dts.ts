c4c100e597e5cda6c9b9273780a93720b8ed839e
JackAtlas <jackatlas8864@gmail.com>
2026-07-08 21:28:01 +0800

    fix: fix generating dts path error

--- scripts/build/generate-dts.ts
+++ scripts/build/generate-dts.ts
@@ -5,8 +5,7 @@ import { $ } from 'zx'
 export async function generateDts(packagePath: string) {
   const tscUrl = await import.meta.resolve('typescript/bin/tsc')
   const tscPath = new URL(tscUrl).pathname
-  const tscPathNormalized = tscPath.startsWith('/') ? tscPath.slice(1) : tscPath
-  await $({ cwd: packagePath })`node ${tscPathNormalized} --project tsconfig.build.json`
+  await $({ cwd: packagePath })`node ${tscPath} --project tsconfig.build.json`
 
   await fs.copy(path.join(packagePath, 'lib/index.d.ts'), path.join(packagePath, 'lib/index.d.mts'))
 }
