bd13d5c6cac83a2a794ce261089053d05cb82a44
JackAtlas <jackatlas8864@gmail.com>
2026-07-09 08:44:43 +0800

    feat: full install

--- packages/@cck-ui/core/src/index.ts
+++ packages/@cck-ui/core/src/index.ts
@@ -1,2 +1,27 @@
+import type { App, Plugin } from 'vue'
+import { CButton } from './components'
+import { CckConfigProvider, CBox } from './core'
+
+const INSTALLED_KEY = Symbol('INSTALLED_KEY')
+
+const components = [CckConfigProvider, CBox, CButton]
+
+const CckUI: Plugin = {
+  install(app: App) {
+    if ((app as any)[INSTALLED_KEY]) {
+      return
+    }
+    ;(app as any)[INSTALLED_KEY] = true
+    components.forEach((c) => {
+      if (c.name) {
+        app.component(c.name, c)
+      }
+    })
+  },
+}
+
+export default CckUI
+export { CckUI }
+
 export * from './components'
 export * from './core'
