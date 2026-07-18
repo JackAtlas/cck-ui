247d97e705a6256e8762f73af24f957f2e0a15e1
JackAtlas <jackatlas8864@gmail.com>
2026-07-18 14:42:20 +0800

    test(vitest): mock window.matchMedia for jsdom

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -2,6 +2,20 @@ import { vi } from 'vitest'
 import { config } from '@vue/test-utils'
 import ResizeObserver from 'resize-observer-polyfill'
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+})
+
 vi.stubGlobal('ResizeObserver', ResizeObserver)
 
 config.global.stubs = {}
