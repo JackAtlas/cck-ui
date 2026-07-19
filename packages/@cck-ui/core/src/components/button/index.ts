d2dc9edd750ddd68c61340ff3103022b03ea1adf
JackAtlas <jackatlas8864@gmail.com>
2026-07-19 22:28:48 +0800

    feat(button): attach classes, varsResolver and extend function to button

--- packages/@cck-ui/core/src/components/button/index.ts
+++ packages/@cck-ui/core/src/components/button/index.ts
@@ -1,17 +1,31 @@
-import { type SFCWithInstall, withInstall } from '../../core'
+import {
+  type SFCWithInstall,
+  type SFCWithInstallAndClasses,
+  withClasses,
+  withExtend,
+  withInstall,
+  withVarsResolver,
+} from '../../core'
 import ButtonGroup from './button-group/button-group.vue'
 import ButtonGroupSection from './button-group-section/button-group-section.vue'
 import Button from './button.vue'
+import classes from './button.module.css'
+
+import { varsResolver } from './button.utils'
+
+const ButtonWithStatic = withExtend(withVarsResolver(withClasses(Button, classes), varsResolver))
 
 export const CButtonGroup: SFCWithInstall<typeof ButtonGroup> = withInstall(ButtonGroup)
 export const CButtonGroupSection: SFCWithInstall<typeof ButtonGroupSection> =
   withInstall(ButtonGroupSection)
-export const CButton: SFCWithInstall<typeof Button> & {
+export const CButton: SFCWithInstallAndClasses<typeof Button, typeof classes> & {
   ButtonGroup: typeof ButtonGroup
-} & { ButtonGroupSection: typeof ButtonGroupSection } = withInstall(Button, {
+  ButtonGroupSection: typeof ButtonGroupSection
+} = withInstall(ButtonWithStatic, {
   ButtonGroup,
   ButtonGroupSection,
 })
+
 export default CButton
 
 export * from './button-group-section/button-group-section.types'
