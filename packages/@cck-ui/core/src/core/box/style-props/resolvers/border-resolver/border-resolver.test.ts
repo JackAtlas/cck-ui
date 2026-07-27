50fd5f9cf9a9823e19826728dca3371491e956d8
JackAtlas <jackatlas8864@gmail.com>
2026-07-27 20:06:40 +0800

    test: testing for borderResolver

---
+++ packages/@cck-ui/core/src/core/box/style-props/resolvers/border-resolver/border-resolver.test.ts
@@ -0,0 +1,34 @@
+import { describe, expect, it } from 'vitest'
+import { borderResolver } from './border-resolver'
+import { DEFAULT_THEME } from '../../../../config-provider'
+import { rem } from '../../../../utils'
+
+describe('@cck-ui/core/Box/border-resolver', () => {
+  it('correctly resolves border with width only', () => {
+    expect(borderResolver(1, DEFAULT_THEME)).toBe(rem(1))
+    expect(borderResolver('1rem', DEFAULT_THEME)).toBe(rem('1rem'))
+  })
+
+  it('correctly resolves border with width and style', () => {
+    expect(borderResolver('1px solid', DEFAULT_THEME)).toBe(`${rem('1px')} solid`)
+    expect(borderResolver('1rem solid', DEFAULT_THEME)).toBe(`${rem('1rem')} solid`)
+  })
+
+  it('correctly resolves border with width, style and color', () => {
+    expect(borderResolver('1px solid red', DEFAULT_THEME)).toBe(
+      `${rem('1px')} solid var(--c-color-red-filled)`
+    )
+    expect(borderResolver('1rem solid red.8', DEFAULT_THEME)).toBe(
+      `${rem('1rem')} solid var(--c-color-red-8)`
+    )
+    expect(borderResolver('1rem solid #000', DEFAULT_THEME)).toBe(`${rem('1rem')} solid #000`)
+
+    expect(borderResolver('1rem solid rgba(0, 0, 0, .12)', DEFAULT_THEME)).toBe(
+      `${rem('1rem')} solid rgba(0, 0, 0, .12)`
+    )
+
+    expect(borderResolver('1px solid red', DEFAULT_THEME)).toBe(
+      `${rem('1px')} solid var(--c-color-red-filled)`
+    )
+  })
+})
