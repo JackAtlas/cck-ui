07a0b778d5f62a6854ec263ad06e0e4d393ed2b0
JackAtlas <jackatlas8864@gmail.com>
2026-07-27 20:11:10 +0800

    test: testing for colorsTuple

---
+++ packages/@cck-ui/core/src/core/config-provider/color-functions/colors-tuple/colors-tuple.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from 'vitest'
+import { colorsTuple } from './colors-tuple'
+
+describe('@cck-ui/core/colorsTuple', () => {
+  it('returns array of 10 colors if argument is a string', () => {
+    expect(colorsTuple('red')).toStrictEqual(Array(10).fill('red'))
+  })
+
+  it('returns array if argument is an array', () => {
+    expect(colorsTuple(['red', 'blue'])).toStrictEqual(['red', 'blue'])
+  })
+})
