96012332392fb58ae10a47767b3aa21dc9f4b06b
JackAtlas <jackatlas8864@gmail.com>
2026-07-27 20:12:04 +0800

    test: testing for getPrimaryShade

---
+++ packages/@cck-ui/core/src/core/config-provider/color-functions/get-primary-shade/get-primary-shade.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest'
+import { getPrimaryShade } from './get-primary-shade'
+import { DEFAULT_THEME } from '../../default-theme'
+
+describe('@cck-ui/core/get-primary-shade', () => {
+  it('returns correct primary shade', () => {
+    expect(getPrimaryShade(DEFAULT_THEME, 'dark')).toBe((DEFAULT_THEME.primaryShade as any).dark)
+    expect(getPrimaryShade(DEFAULT_THEME, 'light')).toBe((DEFAULT_THEME.primaryShade as any).light)
+    expect(getPrimaryShade(DEFAULT_THEME, 'auto')).toBe((DEFAULT_THEME.primaryShade as any).light)
+  })
+})
