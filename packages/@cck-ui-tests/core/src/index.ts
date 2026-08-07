b9132378d4f9e22c09e6e13ab0e00d69d40d64f4
JackAtlas <jackatlas8864@gmail.com>
2026-08-07 19:22:29 +0800

    feat: export ref test

--- packages/@cck-ui-tests/core/src/index.ts
+++ packages/@cck-ui-tests/core/src/index.ts
@@ -1,7 +1,8 @@
 import { axe } from './axe'
 import { itSupportsSystemProps } from './it-supports-system-props'
 import { itSupportsFocusEvent } from './shared/it-supports-focus-events'
+import { itSupportsRef } from './shared/it-supports-ref'
 
-export const tests = { itSupportsFocusEvent, itSupportsSystemProps, axe }
+export const tests = { itSupportsRef, itSupportsFocusEvent, itSupportsSystemProps, axe }
 export { render, renderWithAct } from './render'
 export { wait } from './wait'
