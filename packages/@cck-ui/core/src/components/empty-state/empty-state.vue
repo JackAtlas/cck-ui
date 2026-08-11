57a306b03948783a127034042de49004ceca595e
JackAtlas <jackatlas8864@gmail.com>
2026-08-11 19:51:46 +0800

    feat(empty-state): actions slot

--- packages/@cck-ui/core/src/components/empty-state/empty-state.vue
+++ packages/@cck-ui/core/src/components/empty-state/empty-state.vue
@@ -11,6 +11,9 @@
         <slot name="description" />
       </empty-state-description>
       <slot />
+      <empty-state-actions v-if="$slots.actions">
+        <slot name="actions" />
+      </empty-state-actions>
     </c-box>
   </c-box>
 </template>
@@ -22,6 +25,7 @@ import { EmptyStateFactory, EmptyStateProps } from './empty-state.types'
 import classes from './empty-state.module.css'
 import { varsResolver } from './empty-state.utils'
 import { provideEmptyStateContext } from './empty-state.context'
+import EmptyStateActions from './empty-state-actions/empty-state-actions.vue'
 import EmptyStateDescription from './empty-state-description/empty-state-description.vue'
 import EmptyStateIndicator from './empty-state-indicator/empty-state-indicator.vue'
 import EmptyStateTitle from './empty-state-title/empty-state-title.vue'
@@ -31,16 +35,16 @@ defineOptions({
 })
 
 defineSlots<{
+  actions: any
   default: any
   description: any
   icon: any
   title: any
-}>
+}>()
 
 const _root = ref<InstanceType<typeof CBox> | null>(null)
 
 const attrs = useAttrs()
-const slots = useSlots()
 
 const rawProps = defineProps<EmptyStateProps>()
 
