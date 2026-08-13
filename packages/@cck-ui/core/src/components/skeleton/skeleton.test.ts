17cf523b69f940ae2d99dfb3d4d0a792275cc54f
JackAtlas <jackatlas8864@gmail.com>
2026-08-14 07:28:22 +0800

    test(skeleton): test file of skeleton component

---
+++ packages/@cck-ui/core/src/components/skeleton/skeleton.test.ts
@@ -0,0 +1,45 @@
+import { describe, it, expect } from 'vitest'
+import { render, tests } from '@cck-ui-tests/core'
+import CSkeleton from '.'
+import { SkeletonProps, SkeletonStylesNames } from './skeleton.types'
+
+const defaultProps: SkeletonProps = {}
+
+describe('@cck-ui/core/skeleton', () => {
+  tests.itSupportsSystemProps<SkeletonProps, SkeletonStylesNames>({
+    component: CSkeleton,
+    props: defaultProps,
+    slots: { default: () => 'test-children' },
+    varsResolver: true,
+    children: true,
+    name: 'CSkeleton',
+    staticName: 'Skeleton',
+    stylesApiSelectors: ['root'],
+  })
+
+  it('sets data-visible attribute based on visible prop', async () => {
+    const { wrapper, rerender } = render(CSkeleton, {
+      props: { visible: false },
+    })
+
+    let root = wrapper.find('.c-Skeleton-root')
+    expect(root.attributes('data-visible')).toBeUndefined()
+
+    await rerender({ visible: true })
+    root = wrapper.find('.c-Skeleton-root')
+    expect(root.attributes('data-visible')).toBeDefined()
+  })
+
+  it('sets data-animate attribute based on animate prop', async () => {
+    const { wrapper, rerender } = render(CSkeleton, {
+      props: { animate: false },
+    })
+
+    let root = wrapper.find('.c-Skeleton-root')
+    expect(root.attributes('data-animate')).toBeUndefined()
+
+    await rerender({ animate: true })
+    root = wrapper.find('.c-Skeleton-root')
+    expect(root.attributes('data-animate')).toBeDefined()
+  })
+})
