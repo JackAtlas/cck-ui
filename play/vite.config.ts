fe5ddf5bc62ea254a545bce12a63cc95c334a591
JackAtlas <jackatlas8864@gmail.com>
2026-08-24 10:46:12 +0800

    chore(vite): add postcss plugins in playground config

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
+import presetCck from 'postcss-preset-cck'
+import simpleVars from 'postcss-simple-vars'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -9,6 +12,22 @@ export default defineConfig({
     alias: {
       '@cck-ui/core': path.resolve(__dirname, '../packages/@cck-ui/core/src/index.ts'),
       '@cck-ui/hooks': path.resolve(__dirname, '../packages/@cck-ui/hooks/src/index.ts'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        ...presetCck({ autoRem: true }),
+        simpleVars({
+          variables: {
+            'c-breakpoint-xs': '36em',
+            'c-breakpoint-sm': '48em',
+            'c-breakpoint-md': '62em',
+            'c-breakpoint-lg': '75em',
+            'c-breakpoint-xl': '88em',
+          },
+        }),
+      ],
     },
   },
 })
