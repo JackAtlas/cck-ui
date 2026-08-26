394a3ee6f9022e577c98ed639d39cc182c5bed36
JackAtlas <jackatlas8864@gmail.com>
2026-08-26 17:10:48 +0800

    feat(test): export itHasExtend test

--- packages/@cck-ui-tests/core/src/index.ts
+++ packages/@cck-ui-tests/core/src/index.ts
@@ -1,8 +1,15 @@
 import { axe } from './axe'
 import { itSupportsSystemProps } from './it-supports-system-props'
+import { itHasExtend } from './shared/it-has-extend'
 import { itSupportsFocusEvent } from './shared/it-supports-focus-events'
 import { itSupportsRef } from './shared/it-supports-ref'
 
-export const tests = { itSupportsRef, itSupportsFocusEvent, itSupportsSystemProps, axe }
+export const tests = {
+  itSupportsRef,
+  itSupportsFocusEvent,
+  itHasExtend,
+  itSupportsSystemProps,
+  axe,
+}
 export { render, renderWithAct } from './render'
 export { wait } from './wait'
