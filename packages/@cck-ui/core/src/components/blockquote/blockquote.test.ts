a068df0c67c7c9bd8bf47fb2288343de08d8ce82
JackAtlas <jackatlas8864@gmail.com>
2026-09-05 14:18:01 +0800

    test(blockquote): test file of blockquote component

---
+++ packages/@cck-ui/core/src/components/blockquote/blockquote.test.ts
@@ -0,0 +1,20 @@
+import { tests } from '@cck-ui-tests/core'
+import { describe } from 'vitest'
+import { BlockquoteProps, BlockquoteStylesNames } from './blockquote.types'
+import CBlockquote from '.'
+
+describe('@cck-ui/core/blockquote', () => {
+  tests.itSupportsSystemProps<BlockquoteProps, BlockquoteStylesNames>({
+    component: CBlockquote,
+    props: {},
+    slots: {
+      default: () => 'Life',
+      icon: () => 'test-icon',
+      cite: () => '- Forrest Gump',
+    },
+    varsResolver: true,
+    children: true,
+    name: 'CBlockquote',
+    staticName: 'Blockquote',
+  })
+})
