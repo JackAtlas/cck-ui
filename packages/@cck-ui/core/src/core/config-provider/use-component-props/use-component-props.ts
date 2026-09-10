e358fb0dea35944e8707978157477babfd0abf8e
JackAtlas <jackatlas8864@gmail.com>
2026-09-10 14:21:10 +0800

    feat(core): check boolean userProps in use-component-props via instance

--- packages/@cck-ui/core/src/core/config-provider/use-component-props/use-component-props.ts
+++ packages/@cck-ui/core/src/core/config-provider/use-component-props/use-component-props.ts
@@ -1,4 +1,4 @@
-import { computed, ComputedRef } from 'vue'
+import { computed, ComputedRef, getCurrentInstance } from 'vue'
 import { useCckTheme } from '../config-provider.context'
 
 export interface UseComponentPropsOptions<
@@ -8,15 +8,41 @@ export interface UseComponentPropsOptions<
   component: string | (string | undefined)[]
   defaultProps: U
   props: T
+
+  /**
+   * The `Boolean` absent props will be cast to `false` by `defineProps`.
+   * See in Vue documentation: https://vuejs.org/guide/components/props.html
+   * So we have to double check these props from instance.
+   */
+  booleanProps?: (keyof T)[]
+}
+
+function toKebab(s: string) {
+  return s.replace(/[A-Z]/g, (m) => `-${m.toLowerCase()}`)
 }
 
 export function useComponentProps<T extends Record<string, any>, U extends Partial<T> | null = {}>(
   options: UseComponentPropsOptions<T, U>
 ): ComputedRef<T> {
-  const { component, defaultProps = {} as U, props } = options
+  const { component, defaultProps = {} as U, props, booleanProps = [] } = options
 
   const theme = useCckTheme()
   const names = Array.isArray(component) ? component : [component]
+  const instance = getCurrentInstance()
+
+  const implicitFalseProps = computed(() => {
+    const vnodeProps = (instance?.vnode.props as Record<string, any>) || {}
+    const result = new Set<string>()
+    for (const key of booleanProps) {
+      const k = key as string
+      const kebab = toKebab(k)
+      const isPassed = k in vnodeProps || kebab in vnodeProps
+      if (!isPassed && props[k] === false) {
+        result.add(k)
+      }
+    }
+    return result
+  })
 
   const contextProps = computed(() => {
     const components = theme.value.components
@@ -38,8 +64,9 @@ export function useComponentProps<T extends Record<string, any>, U extends Parti
 
   const mergedProps = computed(() => {
     const filteredProps: Partial<T> = {}
+    const implicit = implicitFalseProps.value
     for (const key in props) {
-      if (props[key] !== undefined) {
+      if (props[key] !== undefined && !implicit.has(key)) {
         filteredProps[key] = props[key]
       }
     }
