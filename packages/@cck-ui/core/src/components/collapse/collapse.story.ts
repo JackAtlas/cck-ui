ce5ab37689811e6cd367ef1ea363b82437714972
JackAtlas <jackatlas8864@gmail.com>
2026-09-14 18:04:12 +0800

    docs(collapse): story file of collapse component

--- packages/@cck-ui/core/src/components/collapse/collapse.story.ts
+++ packages/@cck-ui/core/src/components/collapse/collapse.story.ts
@@ -51,13 +51,13 @@ export const Usage: Story = {
         <c-button mb="md" @click="toggle">Toggle content</c-button>
 
         <c-collapse :expanded="opened" :keep-mounted="false">
-          <p>
+          <c-typography>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
             exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
             occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-          </p>
+          </c-typography>
         </c-collapse>
       </div>
     `,
